ea094d8cd40a7ea89a6a53b7190a2637878077ea
andersbor <anbo@easj.dk>
2018-09-21 12:32:44 +0200

    hideShow added

--- src/js/index.ts
+++ src/js/index.ts
@@ -8,5 +8,29 @@ function greeter(person: Person): string {
 }
 let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let element: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+element.innerHTML = "<h2>" + greeter(user) + " and Anders" + "</h2>";
+
+let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("nameButton");
+button.addEventListener("click", doIt);
+
+function doIt(): void {
+    let element: HTMLInputElement = <HTMLInputElement>document.getElementById("nameInput");
+    let name: string = element.value;
+
+    let divElement: HTMLDivElement = <HTMLDivElement>document.getElementById("nameOutput");
+    divElement.innerHTML = "Hallo " + name;
+
+}
+
+let hideShowButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("hideShowButton");
+hideShowButton.addEventListener("click", () => {
+    let textElement: HTMLDivElement = <HTMLDivElement>document.getElementById("textId");
+    if (textElement.style.display === "none") {
+        textElement.style.display = "block";
+        hideShowButton.innerText = "Hide";
+    } else {
+        textElement.style.display = "none";
+        hideShowButton.innerText = "Show";
+    }
+});
